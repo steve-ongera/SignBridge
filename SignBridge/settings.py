2f3f37439e7b71650f1240adcf71b03abe9d217e
steve-ongera <gadafisteve001@gmail.com>
2026-02-19 23:42:09 +0300

    updats

--- SignBridge/settings.py
+++ SignBridge/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'SignBridge.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
